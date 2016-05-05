94a62e2ce75221be2e0a3d717bf0c5998174b36d
Leedabord <gwforumlakers@gmail.com>
2016-05-04 22:15:28 -0700

    offline changes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 function(){
   var temp = 0;
-/*
+
   var gem = { name: 'Azurite', price: 2.95 };
   var app = angular.module('gemStore', []);
   
   app.controller("StoreController", function() {
     this.product = gem;
   });  
-*/
   
 })();
+
+function SimpleController($scope) {
+  $scope.customers = [ { name: 'Dave Jones', city: 'Phoenix' },  { name: 'Jaime Riley', city: 'Atlanta' },  { name: 'Tomas Walter', city: 'New York' },  { name: 'Heedy Wahlin', city: 'Seattle' },  { name: 'Joe Torres', city: 'Los Angeles' }, ];
+}
