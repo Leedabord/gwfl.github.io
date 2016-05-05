7ef276f2b12c003015c6d6207b0bcf681ce37689
Leedabord <gwforumlakers@gmail.com>
2016-05-04 19:21:37 -0700

    Br0 (#10)
    * Update script
    
    * Update script

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 function(){
+  var temp = 0;
+/*
   var gem = { name: 'Azurite', price: 2.95 };
   var app = angular.module('gemStore', []);
   
   app.controller("StoreController", function() {
     this.product = gem;
-  });
+  });  
+*/
   
 })();
